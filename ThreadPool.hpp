43232fb9284ee654670bade6a20ba39258a46b9b
msupernaw <matthew.supernaw@noaa.gov>
2018-02-16 12:42:35 -0500

    Update ThreadPool.hpp

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,7 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author  Matthew R. Supernaw
+ *
+ * Public Domain Notice
+ * National Oceanic And Atmospheric Administration
+ *
+ * This software is a "United States Government Work" under the terms of the
+ * United States Copyright Act.  It was written as part of the author's official
+ * duties as a United States Government employee and thus cannot be copyrighted.
+ * This software is freely available to the public for use. The National Oceanic
+ * And Atmospheric Administration and the U.S. Government have not placed any
+ * restriction on its use or reproduction.  Although all reasonable efforts have
+ * been taken to ensure the accuracy and reliability of the software and data,
+ * the National Oceanic And Atmospheric Administration and the U.S. Government
+ * do not and cannot warrant the performance or results that may be obtained 
+ * by using this software or data. The National Oceanic And Atmospheric 
+ * Administration and the U.S. Government disclaim all  warranties, express 
+ * or implied, including warranties of performance, merchantability or fitness
+ * for any particular purpose.
+ *
+ * Please cite the author(s) in any work or product based on this material.
+ *
  */
 
 /* 
