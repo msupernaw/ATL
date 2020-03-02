841cd9a18b381946a78e69a36897f0ada06cc651
mattadmin <mattadmin@172.31.99.178>
2020-03-02 15:27:18 -0500

    updated file structure

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -17,7 +17,7 @@ RANLIB=ranlib
 CC=gcc
 CCC=g++
 CXX=g++
-FC=gfortran
+FC=g77
 AS=as
 
 # Macros
@@ -34,7 +34,30 @@ include Makefile
 OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES= \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkAlgorithm.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkBitset.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkDeque.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHash.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHeap.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkList.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkMap.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSet.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSort.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkString.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkTupleVector.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkVector.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/EASTLBenchmark.o \
+	${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/main.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/allocator_eastl.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/assert.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/fixed_pool.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/hashtable.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/intrusive_list.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/numeric_limits.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/red_black_tree.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/string.o \
+	${OBJECTDIR}/lib/third_party/EASTL/source/thread_support.o
 
 
 # C Compiler Flags
@@ -59,7 +82,122 @@ LDLIBSOPTIONS=
 
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atl: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atl ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atl ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkAlgorithm.o: lib/third_party/EASTL/benchmark/source/BenchmarkAlgorithm.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkAlgorithm.o lib/third_party/EASTL/benchmark/source/BenchmarkAlgorithm.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkBitset.o: lib/third_party/EASTL/benchmark/source/BenchmarkBitset.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkBitset.o lib/third_party/EASTL/benchmark/source/BenchmarkBitset.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkDeque.o: lib/third_party/EASTL/benchmark/source/BenchmarkDeque.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkDeque.o lib/third_party/EASTL/benchmark/source/BenchmarkDeque.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHash.o: lib/third_party/EASTL/benchmark/source/BenchmarkHash.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHash.o lib/third_party/EASTL/benchmark/source/BenchmarkHash.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHeap.o: lib/third_party/EASTL/benchmark/source/BenchmarkHeap.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkHeap.o lib/third_party/EASTL/benchmark/source/BenchmarkHeap.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkList.o: lib/third_party/EASTL/benchmark/source/BenchmarkList.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkList.o lib/third_party/EASTL/benchmark/source/BenchmarkList.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkMap.o: lib/third_party/EASTL/benchmark/source/BenchmarkMap.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkMap.o lib/third_party/EASTL/benchmark/source/BenchmarkMap.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSet.o: lib/third_party/EASTL/benchmark/source/BenchmarkSet.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSet.o lib/third_party/EASTL/benchmark/source/BenchmarkSet.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSort.o: lib/third_party/EASTL/benchmark/source/BenchmarkSort.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkSort.o lib/third_party/EASTL/benchmark/source/BenchmarkSort.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkString.o: lib/third_party/EASTL/benchmark/source/BenchmarkString.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkString.o lib/third_party/EASTL/benchmark/source/BenchmarkString.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkTupleVector.o: lib/third_party/EASTL/benchmark/source/BenchmarkTupleVector.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkTupleVector.o lib/third_party/EASTL/benchmark/source/BenchmarkTupleVector.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkVector.o: lib/third_party/EASTL/benchmark/source/BenchmarkVector.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/BenchmarkVector.o lib/third_party/EASTL/benchmark/source/BenchmarkVector.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/EASTLBenchmark.o: lib/third_party/EASTL/benchmark/source/EASTLBenchmark.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/EASTLBenchmark.o lib/third_party/EASTL/benchmark/source/EASTLBenchmark.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/main.o: lib/third_party/EASTL/benchmark/source/main.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/benchmark/source/main.o lib/third_party/EASTL/benchmark/source/main.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/allocator_eastl.o: lib/third_party/EASTL/source/allocator_eastl.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/allocator_eastl.o lib/third_party/EASTL/source/allocator_eastl.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/assert.o: lib/third_party/EASTL/source/assert.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/assert.o lib/third_party/EASTL/source/assert.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/fixed_pool.o: lib/third_party/EASTL/source/fixed_pool.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/fixed_pool.o lib/third_party/EASTL/source/fixed_pool.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/hashtable.o: lib/third_party/EASTL/source/hashtable.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/hashtable.o lib/third_party/EASTL/source/hashtable.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/intrusive_list.o: lib/third_party/EASTL/source/intrusive_list.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/intrusive_list.o lib/third_party/EASTL/source/intrusive_list.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/numeric_limits.o: lib/third_party/EASTL/source/numeric_limits.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/numeric_limits.o lib/third_party/EASTL/source/numeric_limits.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/red_black_tree.o: lib/third_party/EASTL/source/red_black_tree.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/red_black_tree.o lib/third_party/EASTL/source/red_black_tree.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/string.o: lib/third_party/EASTL/source/string.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/string.o lib/third_party/EASTL/source/string.cpp
+
+${OBJECTDIR}/lib/third_party/EASTL/source/thread_support.o: lib/third_party/EASTL/source/thread_support.cpp
+	${MKDIR} -p ${OBJECTDIR}/lib/third_party/EASTL/source
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/lib/third_party/EASTL/source/thread_support.o lib/third_party/EASTL/source/thread_support.cpp
 
 # Subprojects
 .build-subprojects:
